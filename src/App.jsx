2664262bfff6cac20e20dcb916aa64d8cc3850fb
Marc Gállego <mga2525@gmail.com>
2023-12-09 14:14:06 +0100

    change "MENU" for a hamburger

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,42 @@ import PropTypes from 'prop-types';
 import aedLogo from '/aed.webp';
 import './App.css';
 
-function TopBarItem({ children, menuOpen }) {
-  return <a className={`top-bar-item${menuOpen ? ' visible' : ''}`} href='#'>{children}</a>;
-}
+const MenuButton = ({ menuOpen, setMenuOpen }) => (
+  <div className={`mobile-menu${menuOpen ? ' menu-open' : ''}`} onClick={() => setMenuOpen((prev) => !prev)}>
+    <div className='bar' />
+    <div className='bar' />
+    <div className='bar' />
+  </div>
+);
+MenuButton.propTypes = {
+  menuOpen: PropTypes.bool.isRequired,
+  setMenuOpen: PropTypes.func.isRequired,
+};
+
+const TopBarItem = ({ children, menuOpen, setMenuOpen }) => (
+  <a onClick={() => setMenuOpen(false)} className={`top-bar-item${menuOpen ? ' visible' : ''}`} href='#'>
+    {children}
+  </a>
+);
 TopBarItem.propTypes = {
   children: PropTypes.string.isRequired,
   menuOpen: PropTypes.bool.isRequired,
+  setMenuOpen: PropTypes.func.isRequired,
 };
 
 function TopBar() {
   const [menuOpen, setMenuOpen] = useState(false);
-  console.log('menuOpen', menuOpen);
 
   return (
     <div className='top-bar'>
       <div className='top-bar-left'>
         <img className='logo' src={aedLogo} alt="AED" />
-        <div className={`mobile-menu${menuOpen ? ' menu-open' : ''}`} onClick={() => setMenuOpen((prev) => !prev)}>Menu</div>
+        <MenuButton menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
       <div className='top-bar-right'>
-        <TopBarItem menuOpen={menuOpen}>About</TopBarItem>
-        <TopBarItem menuOpen={menuOpen}>Projects</TopBarItem>
-        <TopBarItem menuOpen={menuOpen}>Join us</TopBarItem>
+        <TopBarItem menuOpen={menuOpen} setMenuOpen={setMenuOpen}>About</TopBarItem>
+        <TopBarItem menuOpen={menuOpen} setMenuOpen={setMenuOpen}>Projects</TopBarItem>
+        <TopBarItem menuOpen={menuOpen} setMenuOpen={setMenuOpen}>Join us</TopBarItem>
       </div>
     </div >
   );
