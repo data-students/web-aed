03acf1989b3acb46e27806a984d6c73acb0a2fda
Laia MP <laiamogasp@gmail.com>
2026-05-08 14:38:14 +0200

    Fix navbar

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import aedLogo from '/aed.webp'; 
 import './TopBar.css';
 
-// Componente para el botón de menú hamburguesa (sin cambios)
 const MenuButton = ({ menuOpen, setMenuOpen }) => (
   <div className={`mobile-menu${menuOpen ? ' menu-open' : ''}`} onClick={() => setMenuOpen((prev) => !prev)}>
     <div className='bar' /> 
@@ -17,7 +16,6 @@ MenuButton.propTypes = {
   setMenuOpen: PropTypes.func.isRequired,
 };
 
-// Componente para los ítems de la barra superior
 const TopBarItem = ({ id, children, menuOpen, setMenuOpen, isHighlight = false }) => (
   <a 
     onClick={() => setMenuOpen(false)} 
@@ -32,13 +30,12 @@ TopBarItem.propTypes = {
   children: PropTypes.string.isRequired,
   menuOpen: PropTypes.bool.isRequired,
   setMenuOpen: PropTypes.func.isRequired,
-  isHighlight: PropTypes.bool, // Nueva prop para el botón resaltado
+  isHighlight: PropTypes.bool,
 };
 
-// Componente de la barra superior principal
 function TopBar() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [selectedLanguage, setSelectedLanguage] = useState('Català'); // Estado para el idioma
+  const [selectedLanguage, setSelectedLanguage] = useState('Català');
 
   return (
     <div className={`top-bar ${menuOpen ? ' solid' : ''}`}>
@@ -50,12 +47,11 @@ function TopBar() {
 
 
         
-        {/* Ítems de navegación normales */}
         <TopBarItem id='' menuOpen={menuOpen} setMenuOpen={setMenuOpen}>Qui som?</TopBarItem>
         <TopBarItem id='objectius' menuOpen={menuOpen} setMenuOpen={setMenuOpen}>Objectius</TopBarItem>
         <TopBarItem id='projectes' menuOpen={menuOpen} setMenuOpen={setMenuOpen}>Projectes</TopBarItem>
         <TopBarItem id='equip' menuOpen={menuOpen} setMenuOpen={setMenuOpen}>Equip</TopBarItem>
-        {/* Botón resaltado con la prop isHighlight */}
+        
         <TopBarItem id='unir-se' menuOpen={menuOpen} setMenuOpen={setMenuOpen} isHighlight>Uneix-te!</TopBarItem>
       </div>
     </div >
